22713bafbd0f6311287e3a79412248f56df2998c
csny <masaono_13@msn.com>
2015-06-18 20:51:04 +0900

    importParseException削除

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -18,7 +18,6 @@ import com.android.volley.Response;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.net.ParseException;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
